e2c109202a73f1ece2f3bcf201fc02fdde3f2d04
akfangus <akfanakfan@naver.com>
2023-02-24 21:04:09 +0900

    회원가입페이지 기능구현. 1. 비밀번호 정규표현식 - 영어,특수문자, 숫자포함 8자이상 2. 비밀번호 일치여부 ------ 추가 구현할 기능 1. 아이디 중복확인 2.  모든 조건 만족시에만 제출 버튼 활성화

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,33 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 export default function SignUpForm() {
+  const [userid, setUserid] = useState("");
+  const [username, setUsername] = useState("");
+  const [userpassword, setUserpassword] = useState("");
+  const [uservalpassword, setUservalpassword] = useState("");
+  const [passwordMatch, setPasswordMatch] = useState(false);
+  const [isValidPassword, setIsValidPassword] = useState(false);
+
+  const passwordRegex =
+    /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).{8,}$/;
+  //   console.log(userid);
+
+  const handlePasswordChange = (e) => {
+    const value = e.target.value;
+    setUserpassword(value);
+    value === uservalpassword
+      ? setPasswordMatch(true)
+      : setPasswordMatch(false);
+
+    passwordRegex.test(value)
+      ? setIsValidPassword(true)
+      : setIsValidPassword(false);
+  };
+
+  const handleConfirmPasswordChange = (e) => {
+    const value = e.target.value;
+    setUservalpassword(value);
+    value === userpassword ? setPasswordMatch(true) : setPasswordMatch(false);
+  };
+
+  const signupSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log("submit!");
+  };
+
   return (
-    <StDiv>
+    <StForm onSubmit={signupSubmitHandler}>
       <h1>회원가입</h1>
       <div>
-        <input type="text" style={{ width: "270px" }} placeholder="아이디" />
-        <button style={{ width: "80px" }}>중복확인</button>
+        <input
+          type="text"
+          style={{ width: "270px" }}
+          placeholder="아이디"
+          value={userid}
+          onChange={(e) => setUserid(e.target.value)}
+        />
+        <button type="button" style={{ width: "80px" }}>
+          중복확인
+        </button>
       </div>
+      중복입니다/아닙니다
       <div>
-        <input type="text" style={{ width: "350px" }} placeholder="비밀번호" />
+        <input
+          type="text"
+          style={{ width: "350px" }}
+          placeholder="이름"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
       </div>
       <div>
         <input
           type="text"
           style={{ width: "350px" }}
-          placeholder="비밀번호확인"
+          placeholder="비밀번호"
+          value={userpassword}
+          onChange={handlePasswordChange}
         />
       </div>
-
+      {isValidPassword ? <p>일치</p> : <p>불일치</p>}
+      <div>
+        <input
+          type="text"
+          style={{ width: "350px" }}
+          placeholder="비밀번호확인"
+          value={uservalpassword}
+          onChange={handleConfirmPasswordChange}
+        />
+      </div>
+      {passwordMatch ? (
+        <p style={{ color: "green" }}>비밀번호 일치!</p>
+      ) : (
+        <p style={{ color: "red" }}>비밀번호 불일치!</p>
+      )}
       <div>
         <button style={{ width: "350px" }}> 제출!</button>
       </div>
-    </StDiv>
+    </StForm>
   );
 }
 
-const StDiv = styled.div`
+const StForm = styled.form`
   margin-top: 100px;
   display: flex;
   justify-content: center;
